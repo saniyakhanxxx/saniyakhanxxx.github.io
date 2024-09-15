da4eb989ed0bb9797c437016c726060b2e850756
saniyakhanxxx <dsouzahmark@gmail.com>
2024-09-14 22:08:03 -0400

    Initial website with images added

---
+++ script.js
@@ -0,0 +1,7 @@
+// Responsive Navigation Menu
+const hamburger = document.querySelector('.hamburger');
+const navMenu = document.querySelector('.nav-menu');
+
+hamburger.addEventListener('click', () => {
+    navMenu.classList.toggle('active');
+});
